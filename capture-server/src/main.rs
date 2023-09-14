fd2883f817311bf5afcf3c5011f273e02689abba
Ellie Huxtable <ellie@posthog.com>
2023-09-14 12:46:53 +0100

    Add some lifetimes and fixes

--- capture-server/src/main.rs
+++ capture-server/src/main.rs
@@ -1,10 +1,23 @@
 use std::env;
 use std::net::SocketAddr;
 
+<<<<<<< HEAD:capture-server/src/main.rs
 use capture::{router, sink, time};
 
 use crate::time::SystemTime;
 use crate::{router, sink};
+=======
+
+mod api;
+mod capture;
+mod event;
+mod router;
+mod sink;
+mod time;
+mod token;
+mod utils;
+mod prometheus;
+>>>>>>> 726e535 (Add some lifetimes and fixes):src/main.rs
 
 #[tokio::main]
 async fn main() {
@@ -12,14 +25,14 @@ async fn main() {
     let address = env::var("ADDRESS").unwrap_or(String::from("127.0.0.1:3000"));
 
     let app = if use_print_sink {
-        router::router(SystemTime {}, sink::PrintSink {}, true)
+        router::router(time::SystemTime {}, sink::PrintSink {}, true)
     } else {
         let brokers = env::var("KAFKA_BROKERS").expect("Expected KAFKA_BROKERS");
         let topic = env::var("KAFKA_TOPIC").expect("Expected KAFKA_TOPIC");
 
         let sink = sink::KafkaSink::new(topic, brokers).unwrap();
 
-        router::router(SystemTime {}, sink, true)
+        router::router(time::SystemTime {}, sink, true)
     };
 
     // initialize tracing
