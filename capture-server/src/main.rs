471cc9e83d29e515e05877dd7468916a5a1afd4d
Ellie Huxtable <ellie@posthog.com>
2023-09-14 13:47:19 +0100

    Initial workspace setup
    I'd like to tidy a few things (dependencies etc), but get this setup
    asap to avoid awkward rebases going forwards.

---
+++ capture-server/src/main.rs
@@ -0,0 +1,34 @@
+use std::env;
+use std::net::SocketAddr;
+
+use capture::{time, sink, router};
+
+#[tokio::main]
+async fn main() {
+    let use_print_sink = env::var("PRINT_SINK").is_ok();
+    let address = env::var("ADDRESS").unwrap_or(String::from("127.0.0.1:3000"));
+
+    let app = if use_print_sink {
+        router::router(time::SystemTime {}, sink::PrintSink {})
+    } else {
+        let brokers = env::var("KAFKA_BROKERS").expect("Expected KAFKA_BROKERS");
+        let topic = env::var("KAFKA_TOPIC").expect("Expected KAFKA_TOPIC");
+
+        let sink = sink::KafkaSink::new(topic, brokers).unwrap();
+
+        router::router(time::SystemTime {}, sink)
+    };
+
+    // initialize tracing
+
+    tracing_subscriber::fmt::init();
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+
+    tracing::info!("listening on {}", address);
+
+    axum::Server::bind(&address.parse().unwrap())
+        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+        .await
+        .unwrap();
+}
